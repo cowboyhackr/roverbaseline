a06307c11d010454fc24fd80d4e5a2a83b4a560f
System Administrator <john.m.mckenna@live.com>
2015-07-13 23:42:47 -0500

    writing servo commands to arduino

--- arduinobridgereverse.py
+++ arduinobridgereverse.py
@@ -3,10 +3,13 @@ import sys
 import string
  
 ser = serial.Serial('/dev/ttyAMA0', 115200)
+counter = 0
 while True :
     try:
-    	print "begin"
-    	ser.write('go right')
+    	print "counter: " + counter
+    	ser.write(counter)
+    	time.sleep(1)
+        counter += 1
         # Read data incoming on the serial line
         data=ser.readline()
         print data
